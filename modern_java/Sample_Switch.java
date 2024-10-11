23e15cdcc1a5d6795dbed9752a2ddf9b5b895328
thandoncube1 <ncubethando20@gmail.com>
2024-10-11 02:04:27 -0400

    Dev: Working with switch statement

---
+++ modern_java/Sample_Switch.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Sample_Switch {
+  public static String process(int input) {
+    return switch(input) {
+      case 1 -> "one";
+      case 2, 3 -> "two or three";
+      case 4 -> {
+        System.out.println("called...");
+        yield "four";
+      }
+      default -> "whatever";
+    };
+  }
+
+  public static void main(String[] args) {
+    System.out.println(process(1));
+    System.out.println(process(2));
+    System.out.println(process(3));
+    System.out.println(process(4));
+  }
+}
