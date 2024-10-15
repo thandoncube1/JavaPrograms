570c2a4b54e18b295377289bd6e299c3d07c4438
thandoncube1 <ncubethando20@gmail.com>
2024-10-15 00:17:15 -0400

    Adding asynch notes - virtual threads.

--- multithreadingAndAsync/Sample.java
+++ multithreadingAndAsync/Sample.java
@@ -9,16 +9,35 @@ public class Sample {
       throw new RuntimeException(ex);
     }
   }
-  
+
   public static CompletableFuture<String> fetchAsync(String path) {
     return CompletableFuture.supplyAsync(() -> fetch(path));
+  }
+
+  public static Void handleException(Throwable throwable) {
+    System.out.println("ERROR: " + throwable);
+    return null;
   }
 
   public static void main(String[] args) {
     fetchAsync("Sample.java")
       .thenApply(text -> text.length())
       .thenAccept(System.out::println)
+      .exceptionally(err -> handleException(err))
       .join();
   }
 }
 
+/* If all is well, go to the next then...
+ * If there is a failure, go the next exceptionally...
+ * */
+
+// Functional programming is awesome when your functions are pure, they do not have
+// side effects.
+//
+// If your code has to deal with side effects or exceptions, functional programming is not
+// ideal. You can't maintain it easily. The more functions and code you need to use.
+//
+// If your code is imperative it turns into a monster and if its sequential it is beautiful.
+//
+// -- Virtual Threads - are super light weight threads
